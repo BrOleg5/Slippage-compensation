82cbfa84b57abdb643ba15cfec124fef9fe29022
BrOleg5 <oleg.brylyov@mail.ru>
2021-03-09 00:56:03 +0700

    Fixed display block icons from Simulink Robotino library.

--- startup.m
+++ startup.m
@@ -1,2 +1,3 @@
 addpath( strcat( getenv('ROBOTINOMATLAB_DIR'), '/blockset' ) );
 addpath( strcat( getenv('ROBOTINOMATLAB_DIR'), '/toolbox' ) );
+addpath( strcat( getenv('ROBOTINOMATLAB_DIR'), '/blockset/images/jpg' ) );

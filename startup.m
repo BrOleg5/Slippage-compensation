296b52bbbe575a8d3021159d7f6839fc4a4ab898
BrOleg5 <oleg.brylyov@mail.ru>
2021-03-09 00:15:50 +0700

    Rollback after bad commit 2f85389ce5cf06ee1305f00a35a59e677f87cbcb. It was added unneeded changes.

--- startup.m
+++ startup.m
@@ -1,3 +1,2 @@
 addpath( strcat( getenv('ROBOTINOMATLAB_DIR'), '/blockset' ) );
 addpath( strcat( getenv('ROBOTINOMATLAB_DIR'), '/toolbox' ) );
-addpath( strcat( getenv('ROBOTINOMATLAB_DIR'), '/blockset/images/jpg' ) );

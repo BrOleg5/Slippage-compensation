456c01d2e815ce9064bdd18aea8989f34e323c64
BrOleg5 <oleg.brylyov@mail.ru>
2021-05-08 12:19:21 +0700

    Do motor model identification again, because saturation was moved from speed to current PI-controller. Also, add the current feedback coefficient. For that create a new Simulink model Motor_model_identification.slx, where identification and comparison with experimental data were done. Result identification from Parameter Estimator save in Result_of_motor_model_identification.mat. Model overshoot was reduced. All old identification results delete. Motor model parameters update in model_parameters.m. Create Motor_model_with_Matlab_interface.slx, which allows run motor model from Matlab. Script compare_motor_model_output_with_object.m run Motor_model_with_Matlab_interface.slx with given inputs and plot comparative diagram of angular speed and current. Script compare_static_and_dynamic_models.m run Dynamic_motor_model_vs_static_motor_model.slx with given inputs and plot comperative voltage, absolute and relative errors diagram. Matlab data file 'comparing static and dynamic models voltage.m' delete because it's no longer needed. Names of inputs and some block names translate to Russian in Motor_model.slx and Dynamic_motor_model_vs_static_motor_model.slx. In Check_state_space_model.slx motor model replaces by motor reference model.

---
+++ scripts/compare_motor_model_output_with_object.m
@@ -0,0 +1,37 @@
+%% Подгодовка данных и запуск моделирования
+load('Robotino_data.mat');
+model_name = 'Motor_model_with_Matlab_interface';
+n = length(motor1_reference.time);
+disturbance_torque = timeseries(M_dist*ones(n,1), motor1_reference.time);
+set_param(model_name, 'StopTime', num2str(motor1_reference.time(end)));
+set_param([model_name, '/Set speed'], 'VariableName', 'motor1_reference');
+set_param([model_name, '/Disturbance torque'], 'VariableName', 'disturbance_torque');
+simOut = sim(model_name);
+speed = simOut.get('speed');
+current = simOut.get('current');
+%% Построение графиков
+leg = ["Эксперимент", "Модель"];
+lim_x_axis = [0, 58.5];
+figure('Name', 'Speed');
+plot(motor1_output);
+grid on;
+hold on;
+plot(speed, 'LineWidth', 1);
+xlabel('Время, с');
+ylabel('Угловая скорость, рад/с');
+legend(leg);
+xlim(lim_x_axis);
+ylim([-120, 120]);
+yticks(-120:20:120);
+
+figure('Name', 'Current');
+plot(motor1_current);
+grid on;
+hold on;
+plot(current, 'LineWidth', 1);
+xlabel('Время, с');
+ylabel('Ток, А');
+legend(leg);
+xlim(lim_x_axis);
+%% Чистка данных
+clear model_name n disturbance_torque simOut speed current leg
--- scripts/compare_static_and_dynamic_models.m
+++ scripts/compare_static_and_dynamic_models.m
@@ -1,17 +1,34 @@
-load('comparing static and dynamic models voltage.mat');
-
+%% Подгодовка данных и запуск моделирования
+model_name = 'Dynamic_motor_model_vs_static_motor_model';
+set_param(model_name, 'StopTime', '0.4');
+set_param([model_name, '/Set speed'], 'Value', '100');
+set_param([model_name, '/Disturbance torque'], 'Value', '0.018');
+simOut = sim(model_name);
+static_model_voltage = simOut.get('static_model_voltage');
+dynamic_model_voltage = simOut.get('dynamic_model_voltage');
+abs_error = dynamic_model_voltage - static_model_voltage;
+rel_error = abs_error/dynamic_model_voltage;
+%% Строим графики
 figure('Name', 'Comparing static and dynamic models voltage');
-plot(voltage.static_mode.static_model);
+plot(static_model_voltage);
 grid on;
 hold on;
-plot(voltage.static_mode.dynamic_model);
+plot(dynamic_model_voltage);
 xlabel('Время, с');
 ylabel('Напряжение, В');
 legend('Статическая модель', 'Динамическая модель');
+ylim([0, 1.05*max([max(static_model_voltage), max(dynamic_model_voltage)])]);
 
-figure('Name', 'Error');
-plot(voltage.static_mode.error, 'Color', 'black');
+figure('Name', 'Abs error');
+plot(abs_error, 'Color', 'black');
 grid on;
 xlabel('Время, с');
 ylabel('Абсолютная ошибка, В');
+title('');
+
+figure('Name', 'Rel error');
+plot(rel_error, 'Color', 'black');
+grid on;
+xlabel('Время, с');
+ylabel('Относительная ошибка, В');
 title('');
